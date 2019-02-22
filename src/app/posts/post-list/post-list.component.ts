0afb6ba523987ccd5e92e184ac565fbb727dd480
ridho-pratama-putra <ridhopratamavd@gmail.com>
2019-02-22 20:44:21 +0700

    2202 20:42
    - 2 way binding
    - create post model
    - create post
    - section 2.24

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit , Input} from '@angular/core';
+import { Post }from '../posts/post.model'
+
+@Component({
+	selector: 'app-post-list',
+	templateUrl: './post-list.component.html',
+	styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+	@Input() posts: Post[] = [
+	// {title: "First post",content: "this is the content of the First post"},
+	// {title: "Second post",content: "this is the content of the Second post"},
+	// {title: "Third post",content: "this is the content of the Third post"}
+	]
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
